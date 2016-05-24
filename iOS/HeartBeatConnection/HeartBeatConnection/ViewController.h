fbdc9ec6c150c70bcdc6bdd152a5b0854191e4da
Dong Nguyen <dongnd1991@gmail.com>
2016-05-24 16:24:46 +0700

    Create new iOS Project

---
+++ iOS/HeartBeatConnection/HeartBeatConnection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeartBeatConnection
+//
+//  Created by Dong Nguyen on 5/24/16.
+//  Copyright © 2016 Dong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/HeartBeatConnection/HeartBeatConnection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HeartBeatConnection
+//
+//  Created by Dong Nguyen on 5/24/16.
+//  Copyright © 2016 Dong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
